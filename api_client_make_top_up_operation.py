92d72acadb7fac46b4621c8736dd584b551da835
Ekaterina Kozina <katty6991@gmail.com>
2026-08-15 22:16:58 +0200

    Применяем Pydantic в HTTP API клиентах.Применяем Pydantic в CardsGatewayHTTPClient

--- api_client_make_top_up_operation.py
+++ api_client_make_top_up_operation.py
@@ -15,7 +15,7 @@ open_debit_card_account_response = accounts_gateway_client.open_debit_card_accou
 print('Open debit card account response:', open_debit_card_account_response)
 
 make_top_up_operation_response = operations_gateway_client.make_top_up_operation(
-    card_id=open_debit_card_account_response.account.cards[0].id,  # Используем атрибуты вместо ключей
+    card_id=open_debit_card_account_response.account.cards[0].id,
     account_id=open_debit_card_account_response['account']['id']
 )
 print('Make top up operation response:', make_top_up_operation_response)
