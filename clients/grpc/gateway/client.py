0b0b0b5db0998442003f6f1fccba3e4fd4acf4a6
Ekaterina Kozina <katty6991@gmail.com>
2026-09-15 18:32:49 +0200

    Подготовка gRPC API клиента для Locust. Реализуем gRPC interceptor для сбора метрик в Locust

--- clients/grpc/gateway/client.py
+++ clients/grpc/gateway/client.py
@@ -1,4 +1,7 @@
-from grpc import Channel, insecure_channel
+from grpc import Channel, insecure_channel, intercept_channel
+from locust.env import Environment
+
+from clients.grpc.interceptors.locust_interceptor import LocustInterceptor
 
 
 def build_gateway_grpc_client() -> Channel:
@@ -9,3 +12,18 @@ def build_gateway_grpc_client() -> Channel:
     """
     return insecure_channel("localhost:9003")
 
+
+def build_gateway_locust_grpc_client(environment: Environment) -> Channel:
+    """
+    Фабричная функция для создания gRPC-канала, адаптированного для Locust.
+    В канал автоматически встраивается интерцептор LocustInterceptor,
+    который регистрирует вызовы в системе метрик Locust.
+
+    :param environment: Среда выполнения Locust (необходима для отправки событий).
+    :return: gRPC-канал с интерцептором, пригодный для нагрузочного тестирования.
+    """
+    locust_interceptor = LocustInterceptor(environment=environment)
+
+    channel = insecure_channel("localhost:9003")
+
+    return intercept_channel(channel, locust_interceptor)
